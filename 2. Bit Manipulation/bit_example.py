c46176b805fd95e19cb2540886fc18fc2d6a73da
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-08-22 10:24:59 -0600

    Add code for more bit manipulation examples

--- 2. Bit Manipulation/bit_example.py
+++ 2. Bit Manipulation/bit_example.py
@@ -31,5 +31,69 @@ def is_even(n: int) -> bool:
     return (n & 1) == 0
 
 
+def is_power_of_2(n: int) -> bool:
+    return (n & (n - 1)) == 0
+
+
+def twos_complement(n: int) -> int:
+    return (~n) + 1
+
+
+def binary(n: int) -> str:
+    if n == 0:
+        return '0'
+    result: list[str] = []
+    while n:
+        result.append(str(n & 1))
+        # if (n & 1) == 1:
+        #     result.append('1')
+        # else:
+        #     result.append('0')
+        n >>= 1
+        # n = n >> 1
+    return ''.join(result[::-1])
+
+
 print(f'{is_even(5) = }')
 print(f'{is_even(2135132513453452) = }')
+print(f'{is_power_of_2(4) = }')
+print(f'{is_power_of_2(512) = }')
+print(f'{is_power_of_2(4096) = }')
+print(f'{is_power_of_2(666) = }')
+print(f'{is_power_of_2(1000) = }')
+print(f'{is_power_of_2(3) = }')
+print(f'{twos_complement(5) = }')
+print(f'{twos_complement(-42) = }')
+print(f'{binary(5) = }')
+print(f'{binary(10) = }')
+print(f'{binary(0) = }')
+print(f'{binary(42) = }')
+print(f'{binary(1024) = }')
+print(f'{binary(1023) = }')
+
+# Value swaping
+
+# Using a temporary var
+x = 5
+y = 8
+print(f'\n{x = }, {y = }')
+t = x
+x = y
+y = t
+print(f'{x = }, {y = }')
+
+# Using parallel assignment (destructuring)
+x = 5
+y = 8
+print(f'\n{x = }, {y = }')
+x, y = y, x
+print(f'{x = }, {y = }')
+
+# XOR value swapping
+x = 5
+y = 8
+print(f'\n{x = }, {y = }')
+x = x ^ y
+y = x ^ y
+x = x ^ y
+print(f'{x = }, {y = }')
