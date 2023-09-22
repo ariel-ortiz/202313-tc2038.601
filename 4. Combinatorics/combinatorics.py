427b71e60286f8eaff4a5cddcae07dbc25e135b4
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-09-22 10:33:30 -0600

    Add code for permutations

--- 4. Combinatorics/combinatorics.py
+++ 4. Combinatorics/combinatorics.py
@@ -22,14 +22,36 @@ def combinations(s: list[C], k: int) -> list[list[C]]:
     return [t for t in power_set(s) if len(t) == k]
 
 
+def insert(x: C, s: list[C], i: int) -> list[C]:
+    return s[:i] + [x] + s[i:]
+
+
+def insert_everywhere(x: C, s: list[C]) -> list[list[C]]:
+    return [insert(x, s, i) for i in range(len(s) + 1)]
+
+
+def permute(s: list[C]) -> list[list[C]]:
+    if s:
+        return sum([insert_everywhere(s[0], t)
+                    for t in permute(s[1:])],
+                   [])
+    else:
+        return [[]]
+
+
+def permutations(s: list[C], k: int) -> list[list[C]]:
+    return sum([permute(t) for t in combinations(s, k)], [])
+
 if __name__ == '__main__':
     from pprint import pprint
-    pprint(sorted_nicely(power_set([])))
-    pprint(sorted_nicely(power_set(['x'])))
-    pprint(sorted_nicely(power_set(['x', 'y'])))
-    pprint(sorted_nicely(power_set(['x', 'y', 'z'])))
-    pprint(sorted_nicely(power_set(['w', 'x', 'y', 'z'])))
-    pprint(sorted_nicely(combinations(['a', 'b', 'c', 'd'], 2)))
-    pprint(sorted_nicely(combinations(['a', 'b', 'c', 'd'], 3)))
-    pprint(sorted_nicely(combinations(['a', 'b', 'c', 'd'], 0)))
-    pprint(sorted_nicely(combinations(['a', 'b', 'c', 'd'], 5)))
+    # pprint(sorted_nicely(power_set([])))
+    # pprint(sorted_nicely(power_set(['x'])))
+    # pprint(sorted_nicely(power_set(['x', 'y'])))
+    # pprint(sorted_nicely(power_set(['x', 'y', 'z'])))
+    # pprint(sorted_nicely(power_set(['w', 'x', 'y', 'z'])))
+    # pprint(sorted_nicely(combinations(['a', 'b', 'c', 'd'], 2)))
+    # pprint(sorted_nicely(combinations(['a', 'b', 'c', 'd'], 3)))
+    # pprint(sorted_nicely(combinations(['a', 'b', 'c', 'd'], 0)))
+    # pprint(sorted_nicely(combinations(['a', 'b', 'c', 'd'], 5)))
+    # pprint(insert('x', ['y', 'z'], 2))
+    pprint(sorted_nicely(permutations(['w', 'x', 'y', 'z'], 5)))
