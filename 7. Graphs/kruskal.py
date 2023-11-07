4845be488fcd6e8feb24305d21723ae74959ea4f
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2023-11-07 10:59:29 -0600

    Complete code for Kruskal's algorithm

--- 7. Graphs/kruskal.py
+++ 7. Graphs/kruskal.py
@@ -23,7 +23,72 @@ class Edge(NamedTuple):
         return hash(self.weight) + hash(self.u) + hash(self.v)
 
 
+def make_heap(graph: WeightedGraph) -> list[Edge]:
+    result: set[Edge] = set()
+    u: str
+    neighbors: set[tuple[str, int]]
+    for u, neighbors in graph.items():
+        v: str
+        weight: int
+        for v, weight in neighbors:
+            result.add(Edge(weight, u, v))
+    queue: list[Edge] = list(result)
+    heapify(queue)
+    return queue
+
+
+def add_edge(graph: WeightedGraph, edge: Edge) -> None:
+    weight, u, v = edge
+    graph[u].add((v, weight))
+    graph[v].add((u, weight))
+
+
+def remove_edge(graph: WeightedGraph, edge: Edge) -> None:
+    weight, u, v = edge
+    graph[u].remove((v, weight))
+    graph[v].remove((u, weight))
+
+
+def has_cycle(
+        initial: str,
+        graph: WeightedGraph,
+        visited: Optional[set[str]] = None,
+        parent: Optional[str] = None) -> bool:
+    if visited is None:
+        visited = set()
+    visited.add(initial)
+    for vertex, _ in graph[initial]:
+        if vertex in visited:
+            if vertex != parent:
+                return True
+        elif has_cycle(vertex, graph, visited, initial):
+            return True
+    return False
+
+
+def kruskal_mst(graph: WeightedGraph) -> tuple[int, WeightedGraph]:
+    queue: list[Edge] = make_heap(graph)
+    result: WeightedGraph = {k: set() for k in graph}
+    remaining_edges: int = len(graph) - 1
+    total: int = 0
+    visited: set[str] = set()
+    while remaining_edges:
+        edge: Edge = heappop(queue)
+        add_edge(result, edge)
+        if (edge.u in visited and edge.v in visited
+                and has_cycle(edge.u, result)):
+            remove_edge(result, edge)
+        else:
+            visited.add(edge.u)
+            visited.add(edge.v)
+            total += edge.weight
+            remaining_edges -= 1
+    return (total, result)
+
+
 if __name__ == '__main__':
+    from pprint import pprint
+
     g1: WeightedGraph = {
         'A': {('B', 4), ('C', 5)},
         'B': {('A', 4)},
@@ -33,12 +98,16 @@ if __name__ == '__main__':
         'F': {('D', 1), ('E', 3)}
     }
 
-    e1: Edge = Edge(4, 'A', 'B')
-    e2: Edge = Edge(5, 'B', 'A')
-    print(e1 == e2)
-    print(hash(e1))
-    print(hash(e2))
-    s: set[Edge] = set()
-    s.add(e1)
-    s.add(e2)
-    print(s)
+    g2: WeightedGraph = {
+        'A': {('B', 1), ('C', 1)},
+        'B': {('A', 1)},
+        'C': {('A', 1)}
+    }
+
+    g3: WeightedGraph = {
+        'A': {('B', 1), ('C', 1)},
+        'B': {('A', 1), ('C', 1)},
+        'C': {('A', 1), ('B', 1)}
+    }
+
+    pprint(kruskal_mst(g3))
